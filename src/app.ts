959014f263b97184f5f5b8f3790a780be0c8ebd7
Faisal <officialfaisal1112@gmail.com>
2023-08-12 10:17:10 +0600

    [DONE] linting code improve

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
-import express, { Application } from 'express'
 import cors from 'cors'
+import express, { Application } from 'express'
 import usersRouter from './app/modules/users/users.route'
 
 const app: Application = express()
