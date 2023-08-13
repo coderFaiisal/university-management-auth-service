79207166babdf40f2c21482efa8887472bc83404
Faisal <officialfaisal1112@gmail.com>
2023-08-13 23:42:15 +0600

    [DONE] advanced error handling

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,21 @@
-import cors from 'cors'
-import express, { Application } from 'express'
-import usersRouter from './app/modules/users/users.route'
+import cors from 'cors';
+import express, { Application } from 'express';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
+import { UserRoutes } from './app/modules/user/user.route';
 
-const app: Application = express()
+const app: Application = express();
 
 //middleware
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/v1/users', usersRouter)
+app.use('/api/v1/users', UserRoutes);
 
-export default app
+// app.get('/', async (req: Request, res: Response) => {
+//   res.send('working')
+// })
+
+app.use(globalErrorHandler);
+
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,47 @@
-import mongoose from 'mongoose'
-import app from './app'
-import config from './config'
-import { errorlogger, logger } from './shared/logger'
+import { Server } from 'http';
+import mongoose from 'mongoose';
+import app from './app';
+import config from './config';
+import { errorlogger, logger } from './shared/logger';
+
+//handle uncaught exception
+process.on('uncaughtException', error => {
+  errorlogger.error(error);
+  process.exit(1);
+});
+
+let server: Server;
 
 async function bootstrap() {
   try {
-    await mongoose.connect(config.database_url as string)
-    logger.info('Database connect successfully')
+    await mongoose.connect(config.database_url as string);
+    logger.info('Database connect successfully');
 
-    app.listen(config.port, () => {
-      logger.info(`Application is listening on port ${config.port}`)
-    })
-  } catch (err) {
-    errorlogger.error('Failed to connect database', err)
+    server = app.listen(config.port, () => {
+      logger.info(`Application is listening on port ${config.port}`);
+    });
+  } catch (error) {
+    errorlogger.error('Failed to connect database', error);
   }
+  //handle unhandle rejection
+  process.on('unhandledRejection', error => {
+    if (server) {
+      server.close(() => {
+        errorlogger.error(error);
+        process.exit(1);
+      });
+    } else {
+      process.exit(1);
+    }
+  });
 }
 
-bootstrap()
+bootstrap();
+
+//handle signal termination
+process.on('SIGTERM', () => {
+  logger.info('SIGTERM is received');
+  if (server) {
+    server.close();
+  }
+});
